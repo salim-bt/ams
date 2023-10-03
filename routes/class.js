267c133a61a64e0e825e855f7636e25191af2034
Salim <salimpradhan77291188@outlook.com>
2023-10-03 20:34:57 +0600

    class controller

--- routes/class.js
+++ routes/class.js
@@ -2,8 +2,11 @@ const express = require('express');
 const {body, validationResult} = require('express-validator');
 const router = express.Router();
 const {v4: uuidV4} = require('uuid');
-const {createClass} = require("../controllers/class.controller");
+const {createClass, getClass, getAllClasses, updateClass, deleteClass} = require("../controllers/class.controller");
 
-router.post("/class", createClass)
-
+router.post("/create-class", createClass)
+router.get('/get-class/:classId',getClass)
+router.get('/get-all-classes',getAllClasses)
+router.put('/update-class',updateClass)
+router.delete('/delete-class',deleteClass)
 module.exports = router;
